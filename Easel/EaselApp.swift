bac781ae9ef4197bef24b2703249f7d474a69807
Oliver Tran <olivertransf@gmail.com>
2025-12-30 15:37:03 -0800

    Initial Commit

---
+++ Easel/EaselApp.swift
@@ -0,0 +1,17 @@
+//
+//  EaselApp.swift
+//  Easel
+//
+//  Created by Oliver Tran on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct EaselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
